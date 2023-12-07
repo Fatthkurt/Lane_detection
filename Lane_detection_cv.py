b6ac9408ded09faf8bbedb13d6bf6b5e16ab220d
Fatih Kurt <81189708+Fatthkurt@users.noreply.github.com>
2023-12-07 19:21:40 +0100

    Update Lane_detection_cv.py

--- Lane_detection_cv.py
+++ Lane_detection_cv.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-# Define a function to apply Canny edge detection and Probabilistic Hough Line Transform
 def detect_lines(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     edges = cv2.Canny(gray, 100, 200, apertureSize=3)
@@ -13,19 +12,16 @@ def detect_lines(frame):
 
     return frame
 
-# Define a function to create a mask for a region of interest
+# create a mask 
 def create_mask(frame):
     height, width = frame.shape[:2]
     mask = np.zeros_like(frame)
 
-    # Define a polygonal contour that covers the road
     vertices = np.array([[(0, 250),(0, height), (width, height), (width*80, height*2)]], dtype=np.int32)
 
 
-    # Fill the contour with white color
     cv2.fillPoly(mask, vertices, (255, 255, 255))
 
-    # Apply a bitwise AND operation between the mask and the frame
     masked = cv2.bitwise_and(frame, mask)
 
     return masked
@@ -36,23 +32,17 @@ cap = cv2.VideoCapture("Videos/test_video.mp4")
 while cap.isOpened():
     ret, frame = cap.read()
 
-    # Check if the frame is valid
     if ret:
-        # Apply a mask to the frame
         frame = create_mask(frame)
 
-        # Detect lines on the frame
         frame = detect_lines(frame)
 
-        # Show the frame
         cv2.imshow('video', frame)
 
-        # Wait for a key press or end of video
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     else:
         break
 
-# Release the video capture and destroy the windows
 cap.release()
 cv2.destroyAllWindows()
